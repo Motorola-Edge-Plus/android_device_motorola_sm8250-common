210d7e9fe4438bf8c057c763383ebebc7cfd0943
Alex Wong <waiw@codeaurora.org>
2019-02-25 14:53:27 -0800

    sm8250-common: Build 64 bit camera service & implementation
    Change-Id: Ia098deaa9ea05b4f30747306b77d63fa4f3f5f90

--- common.mk
+++ common.mk
@@ -40,6 +40,11 @@ PRODUCT_PACKAGES += \
     bootctrl.kona \
     bootctrl.kona.recovery
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
