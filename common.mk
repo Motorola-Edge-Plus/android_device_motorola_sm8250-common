a0179d809df50261cafd719831f018bce33cdb44
Alexander Koskovich <zvnexus@outlook.com>
2021-08-06 01:25:53 -0700

    sm8250-common: Build QTI camera interfaces for CamX
    Change-Id: I23efb141c135c279f4bfb5c6f0b6c95b6c78a91d

--- common.mk
+++ common.mk
@@ -43,7 +43,9 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64
+    android.hardware.camera.provider@2.4-service_64 \
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor
 
 # DRM
 PRODUCT_PACKAGES += \
