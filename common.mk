7c7843e91f8023b7acdd4ccafe1cf60b2034a1ad
Alexander Koskovich <akoskovich@pm.me>
2022-04-11 00:02:42 -0700

    sm8350-common: Import scripts from LA.UM.9.12.r1-14100.02-SMxx50.QSSI12.0
    Change-Id: Ia624b92cbbaeaf240958df2f640140d79f9f9335

--- common.mk
+++ common.mk
@@ -8,5 +8,20 @@
 # any potential libraries, configurations, etc from other inherited repositories.
 $(call inherit-product, vendor/motorola/sm8250-common/sm8250-common-vendor.mk)
 
+# Init
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
+
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    init.class_main.sh \
+    init.mdm.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.rc \
+    init.qcom.recovery.rc \
+    init.qcom.sh \
+    init.target.rc \
+    ueventd.qcom.rc
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
