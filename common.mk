59263a9e827fffefb4a3cb93c7f390c9afe618bf
Alexander Koskovich <akoskovich@pm.me>
2022-04-28 23:17:47 -0700

    sm8250-common: Add libraries to resolve dependencies blocking build
    Change-Id: I81b8c675d43f14ad4257046fc3cf38506d552419

--- common.mk
+++ common.mk
@@ -4,5 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from our proprietary vendor. We have this at the top so it can override
+# any potential libraries, configurations, etc from other inherited repositories.
+$(call inherit-product, vendor/motorola/sm8250-common/sm8250-common-vendor.mk)
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
