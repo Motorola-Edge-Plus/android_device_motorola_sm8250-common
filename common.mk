cd02749f503d8dc57e46ef4bf63b87a637ce3415
Alexander Koskovich <akoskovich@pm.me>
2022-04-29 15:04:38 -0700

    sm8250-common: Add post install configurations for A/B
    Change-Id: I2659f180ec3d8791a1609fbde944fd9f0dde3987

--- common.mk
+++ common.mk
@@ -8,6 +8,23 @@
 # any potential libraries, configurations, etc from other inherited repositories.
 $(call inherit-product, vendor/motorola/sm8250-common/sm8250-common-vendor.mk)
 
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
+PRODUCT_PACKAGES += \
+    checkpoint_gc \
+    otapreopt_script
+
 # Init
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
