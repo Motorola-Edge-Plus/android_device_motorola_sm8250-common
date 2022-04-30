ab3004f0e36eca7e90d46f358ad38dbc76c27f22
Alexander Koskovich <akoskovich@pm.me>
2022-04-30 00:15:47 -0700

    sm8250-common: Import compatibility matrix from LA.UM.9.12.r1-14100.02-SMxx50.QSSI12.0
    Change-Id: I200a879236ee72fc889afcea0fe376bebfbd4ab7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,6 +87,7 @@ TARGET_KERNEL_SOURCE := kernel/motorola/sm8250
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 
 # Media
 TARGET_USES_ION := true
