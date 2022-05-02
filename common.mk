1cdc6858edba0c38c0538735c7f05e3a0a3d7d25
Alexander Koskovich <akoskovich@pm.me>
2022-05-02 02:03:09 -0700

    sm8250-common: Build libgui_vendor
     * Required by Motorola camera libraries.
    
    Change-Id: I23874b8d98f42786e8ac932dcc7305cbfc683bc9

--- common.mk
+++ common.mk
@@ -86,6 +86,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    libgui_vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
