c6b7642357986adcbc465f798ec1bd9820f300ae
Cosmin Tanislav <demonsingur@gmail.com>
2022-05-02 12:47:04 +0300

    sm8250-common: build fingerprint hidl interface lib
    Needed by android.hardware.biometrics.fingerprint@2.1-goodixservice.
    
    Change-Id: I24ea4b66ec2c524689c890814771ddf8eed38e86

--- common.mk
+++ common.mk
@@ -131,6 +131,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1.vendor
+
 # GPS
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
