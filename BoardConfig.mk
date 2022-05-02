af625ecbc2c2c9b38974799d3d7fc6580826efd2
Alexander Koskovich <akoskovich@pm.me>
2022-05-02 02:04:51 -0700

    sm8250-common: Add Motorola vendor framework compatibility matrix
     * For the addition of Goodix VINTF fragment to proprietary
       files.
    
    Change-Id: If9d86fb1db49e479fc2aea26531a8d265b9e69e8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,6 +87,7 @@ TARGET_KERNEL_SOURCE := kernel/motorola/sm8250
 
 # Manifest
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/moto_vfcm.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
