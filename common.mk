ee19a7367442f49430bc57848158a33fc7cfde5b
Cosmin Tanislav <demonsingur@gmail.com>
2022-05-03 20:19:45 +0300

    sm8250-common: add telephony packages
    Change-Id: I7b773a66efdc7376755f8c4dd1a823ab5ef0061a

--- common.mk
+++ common.mk
@@ -277,6 +277,26 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 PRODUCT_CHARACTERISTICS := nosdcard
 
 # Telephony
+PRODUCT_PACKAGES += \
+    extphonelib \
+    extphonelib-product \
+    extphonelib.xml \
+    extphonelib_product.xml \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti-telephony-hidl-wrapper-prd \
+    qti_telephony_hidl_wrapper.xml \
+    qti_telephony_hidl_wrapper_prd.xml \
+    qti-telephony-utils \
+    qti-telephony-utils-prd \
+    qti_telephony_utils.xml \
+    qti_telephony_utils_prd.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
