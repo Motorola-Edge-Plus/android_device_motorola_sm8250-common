4a91d490ead174932c8169097bf3cc78c5e34b8c
Cosmin Tanislav <demonsingur@gmail.com>
2022-05-03 23:00:21 +0300

    Revert "sm8250-common: Import the QTI AIDL lights HAL"
    This reverts commit d0d53898f246527d2f96961d409d3ba0a20f867f.
    
    Change-Id: Ib540d03dc8d8b68fede8c6d31c4b692c7fb69626

--- common.mk
+++ common.mk
@@ -134,10 +134,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.lights-service.qti \
     gralloc.kona \
     libtinyxml \
-    lights.kona \
     memtrack.kona
 
 # Fastboot
