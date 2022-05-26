f469967fe48b12507a94dd7d260668a212eb3289
Cosmin Tanislav <demonsingur@gmail.com>
2022-05-26 19:03:42 +0300

    sm8250-common: add moto telephony blobs
    Change-Id: Ib8981b8ce5437498bdaa84064300cabafe09b022

--- common.mk
+++ common.mk
@@ -305,6 +305,9 @@ PRODUCT_BOOT_JARS += \
     telephony-ext
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/moto-telephony.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/moto-telephony.xml
+
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
