99a9a005cf0446485bde722fccbd3f765aaa589a
Cosmin Tanislav <demonsingur@gmail.com>
2022-06-03 16:31:45 +0300

    sm8250-common: build vendor version of libtinyalsa for libspeakerbundle
    Change-Id: Iad710195ae7b983de16f6d403b6606d33d9f35bf

--- common.mk
+++ common.mk
@@ -52,6 +52,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
 PRODUCT_PACKAGES += \
+    libtinyalsa.vendor \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     sound_trigger.primary.kona
