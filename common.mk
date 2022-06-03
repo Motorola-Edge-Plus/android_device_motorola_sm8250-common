a8ac3c050b32877cf862b6b650667179345e433a
Cosmin Tanislav <demonsingur@gmail.com>
2022-06-03 16:34:00 +0300

    sm8250-common: import CAF audio effects
    Change-Id: I2fd6227bea75545cd2261d442545672fd82f8011

--- common.mk
+++ common.mk
@@ -39,6 +39,7 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
